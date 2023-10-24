ef78bba0aaec104c01a90007f6dfa003c71d5039
Kyle Roe <hopeman15@users.noreply.github.com>
2023-10-24 22:19:03 +0200

    Setup (#2)
    * update gitignore
    
    * android app
    
    * code of conduct update
    
    * quality gates
    
    * worklow
    
    * readme updates
    
    ---------
    
    Co-authored-by: Kyle <[hopeman15@users.noreply.github.com]>

--- Makefile
+++ Makefile
@@ -1,3 +1,39 @@
-# TODO: add commands here
-# build:
-# .PHONY: build
+CI ?= false
+BUILD_TYPE ?= Debug
+GRADLE_ARGS ?= --build-cache
+
+ifeq ($(CI), true)
+  GRADLE_ARGS += --console 'plain'
+  BUILD_TYPE = Release
+endif
+
+all: clean format lint test report assemble
+.PHONY: all
+
+assemble:
+	./gradlew assemble${BUILD_TYPE} ${GRADLE_ARGS}
+.PHONY: assemble
+
+bundle:
+	./gradlew bundle${BUILD_TYPE} ${GRADLE_ARGS}
+.PHONY: bundle
+
+clean:
+	./gradlew clean ${GRADLE_ARGS}
+.PHONY: clean
+
+format:
+	./gradlew formatKotlin ${GRADLE_ARGS}
+.PHONY: format
+
+lint:
+	./gradlew lint${BUILD_TYPE} lintKotlin detekt ${GRADLE_ARGS}
+.PHONY: lint
+
+report:
+	./gradlew koverHtmlReport koverXmlReport ${GRADLE_ARGS}
+.PHONY: report
+
+test:
+	./gradlew test${BUILD_TYPE}UnitTest ${GRADLE_ARGS}
+.PHONY: test
---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    id("org.jmailen.kotlinter") version "3.16.0"
+    id("org.jetbrains.kotlinx.kover") version "0.7.4"
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jmailen.kotlinter")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+
+    detekt {
+        buildUponDefaultConfig = false
+        allRules = false
+        config.setFrom("$rootDir/detekt/default-detekt-config.yml")
+    }
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = "17"
+        reports {
+            xml.required.set(false)
+            html.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+        }
+        exclude("**/resources/**")
+        exclude("**/build/**")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+koverReport {
+    filters {
+        excludes {
+            classes("*BuildConfig")
+            annotatedBy("*Preview")
+        }
+    }
+
+    defaults {
+        xml {
+            onCheck = false
+            setReportFile(layout.buildDirectory.file("$buildDir/reports/kover/result.xml"))
+        }
+        html {
+            onCheck = false
+            setReportDir(layout.buildDirectory.dir("$buildDir/reports/kover/html-result"))
+        }
+    }
+}
