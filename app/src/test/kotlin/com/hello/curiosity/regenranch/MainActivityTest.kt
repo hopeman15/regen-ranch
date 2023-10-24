bb93cb4863c321ac88f5da835befe4de0d85bf2c
Kyle <[hopeman15@users.noreply.github.com]>
2023-10-24 23:08:26 +0200

    [Testing] MainActivity

---
+++ app/src/test/kotlin/com/hello/curiosity/regenranch/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package com.hello.curiosity.regenranch
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [33])
+class MainActivityTest {
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testActivityExists() {
+        val activity = activityScenarioRule.scenario
+        activity.moveToState(Lifecycle.State.CREATED)
+    }
+}
